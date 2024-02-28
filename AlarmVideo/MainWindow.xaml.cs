ecf292ac1ccc5e2ebe7c462aefc25947d11b6233
KaarelNoole <kaarelnoole@gmail.com>
2024-02-28 14:06:47 +0200

    Add marker to map

--- AlarmVideo/MainWindow.xaml.cs
+++ AlarmVideo/MainWindow.xaml.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using VideoOS.Platform;
@@ -10,16 +9,11 @@ using VideoOS.Platform.Data;
 using VideoOS.Platform.Messaging;
 using VideoOS.Platform.UI;
 using VideoOS.Platform.UI.Controls;
-//using Milestone.XProtect.Client.UI.Maps;
-//using Milestone.XProtect.Client.UI.Maps.MapService;
-using VideoOS.Platform.ConfigurationItems;
-using MapControl;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using System.Security.Cryptography;
 using Microsoft.Maps.MapControl.WPF;
-using GMap.NET.MapProviders;
-using System.Net;
-//using System.Device.Location;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using MapControl;
+using Location = Microsoft.Maps.MapControl.WPF.Location;
 
 namespace AlarmVideo
 {
@@ -30,65 +24,16 @@ namespace AlarmVideo
         public MainWindow()
         {
             InitializeComponent();
-            // Set the initial coordinates for the camera
-            double initialLatitude = 59.310565; // Example latitude
-            double initialLongitude = 24.429899; // Example longitude
-            double initialZoomLevel = 18; // Example zoom level
 
-            //// Set the camera position
-            Microsoft.Maps.MapControl.WPF.Location initialLocation = new Microsoft.Maps.MapControl.WPF.Location(initialLatitude, initialLongitude);
+            double initialLatitude = 58.883333;
+            double initialLongitude = 25.557222;
+            double initialZoomLevel = 6;
+            Location initialLocation = new Location(initialLatitude, initialLongitude);
+
             mapControl.Center = initialLocation;
             mapControl.ZoomLevel = initialZoomLevel;
+
         }
-        //private void InitializeMap()
-        //{
-
-        //    gmapControl.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
-        //    GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
-        //    gmapControl.Position = new GMap.NET.PointLatLng(0, 0);
-        //    gmapControl.Zoom = 13;
-        //}
-
-        //public void ConnectToServer()
-        //{
-        //    // Connect to the Milestone Management Server
-        //    MIP.Initialize();
-        //    MIP.Server.Connect("serverAddress", "username", "password");
-
-        //    // Retrieve camera information
-        //    Camera[] cameras = MIP.System.GetCameras();
-
-        //    // Display camera coordinates on the map
-        //    foreach (var camera in cameras)
-        //    {
-        //        double latitude = camera.Latitude;
-        //        double longitude = camera.Longitude;
-
-        //        // Add code to display markers on the map using latitude and longitude
-        //    }
-
-        //    // Set camera coordinates (this is a simplified example, actual implementation may vary)
-        //    Camera targetCamera = cameras[0];
-        //    targetCamera.Latitude = newLatitude;
-        //    targetCamera.Longitude = newLongitude;
-        //    targetCamera.Update();
-        //}
-
-        //private void AddMarkerToMap(double latitude, double longitude, string title)
-        //{
-        //    TextAnnotation textAnnotation = new TextAnnotation
-        //    {
-        //        Location = new Geopoint(new BasicGeoposition
-        //        {
-        //            Latitude = latitude,
-        //            Longitude = longitude
-        //        }),
-        //        Text = title
-        //    };
-
-        //    // Add the text annotation to the map
-        //    mapView.Children.Add(textAnnotation);
-        //}
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
@@ -258,39 +203,51 @@ namespace AlarmVideo
                 Items = Configuration.Instance.GetItems()
             };
 
-            //if (_selectItem1 != null)
-            //{
-            //    // Update the map with the camera location
-            //    //double cameraLatitude = _selectItem1.Position.Latitude;
-            //    //double cameraLongitude = _selectItem1.Position.Longitude;
-
-            //    // Set the camera location on the map
-            //    mapControl.Center = new Microsoft.Maps.MapControl.WPF.Location(cameraLatitude, cameraLongitude);
-
-            //    // Optionally, you can set a marker or other visual representation on the map
-            //    // based on the camera location.
-            //    // For example, you might use a Pushpin from the Bing Maps control.
-            //    var pushpin = new Microsoft.Maps.MapControl.WPF.Pushpin();
-            //    pushpin.Location = new Microsoft.Maps.MapControl.WPF.Location(cameraLatitude, cameraLongitude);
-            //    mapControl.Children.Add(pushpin);
-            //}
 
             if (itemPicker.ShowDialog().Value)
             {
+
+                //double initialLatitude = _selectItem1.Latitude ?? 0;
+                //double initialLongitude = _selectItem1.Longitude ?? 0; 
+                //double initialZoomLevel = 16;
+                double initialLatitude = 59.310565;
+                double initialLongitude = 24.429899;
+                double initialZoomLevel = 16;
+
+                // Set the initial camera position
+                Microsoft.Maps.MapControl.WPF.Location initialLocation = new Microsoft.Maps.MapControl.WPF.Location(initialLatitude, initialLongitude);
+
+                // Set the map control properties
+                mapControl.Center = initialLocation;
+                mapControl.ZoomLevel = initialZoomLevel;
+
+                // Create a custom marker (Image)
+                var customIcon = new BitmapImage(new Uri("/icon/icon.png", UriKind.Relative));
+
+                var marker = new Image
+                {
+                    Source = customIcon,
+                    Width = 40,
+                    Height = 40
+                };
+
+                // Set the location for the marker on the map
+                MapLayer.SetPosition(marker, initialLocation);
+
+                // Add the marker to the map's children
+                mapControl.Children.Add(marker);
+
+
                 _selectItem1 = itemPicker.SelectedItems.First();
-                buttonSelect1.Content = _selectItem1.Name;
-
-                _imageViewerWpfControl.CameraFQID = _selectItem1.FQID;
-                // Lets enable/disable the header based on the tick mark. Could also disable LiveIndicator or CameraName.
-                _imageViewerWpfControl.EnableVisibleHeader = checkBoxHeader.IsChecked.Value;
-                _imageViewerWpfControl.EnableVisibleLiveIndicator = EnvironmentManager.Instance.Mode == Mode.ClientLive;
-                _imageViewerWpfControl.AdaptiveStreaming = checkBoxAdaptiveStreaming.IsChecked.Value;
-
-                _imageViewerWpfControl.Initialize();
-                _imageViewerWpfControl.Connect();
-                _imageViewerWpfControl.Selected = true;
-
-                _imageViewerWpfControl.EnableDigitalZoom = checkBoxDigitalZoom.IsChecked.Value;
+                    buttonSelect1.Content = _selectItem1.Name;
+                    _imageViewerWpfControl.CameraFQID = _selectItem1.FQID;
+                    _imageViewerWpfControl.EnableVisibleHeader = checkBoxHeader.IsChecked.Value;
+                    _imageViewerWpfControl.EnableVisibleLiveIndicator = EnvironmentManager.Instance.Mode == Mode.ClientLive;
+                    _imageViewerWpfControl.AdaptiveStreaming = checkBoxAdaptiveStreaming.IsChecked.Value;
+                    _imageViewerWpfControl.Initialize();
+                    _imageViewerWpfControl.Connect();
+                    _imageViewerWpfControl.Selected = true;
+                    _imageViewerWpfControl.EnableDigitalZoom = checkBoxDigitalZoom.IsChecked.Value;
             }
         }
 
@@ -311,14 +268,14 @@ namespace AlarmVideo
         {
             if (_selectItem1 != null)
                 EnvironmentManager.Instance.PostMessage(
-                    new VideoOS.Platform.Messaging.Message(MessageId.Control.StartRecordingCommand), _selectItem1.FQID);
+                    new Message(MessageId.Control.StartRecordingCommand), _selectItem1.FQID);
         }
 
         private void ButtonStopRecording1_Click(object sender, RoutedEventArgs e)
         {
             if (_selectItem1 != null)
                 EnvironmentManager.Instance.PostMessage(
-                    new VideoOS.Platform.Messaging.Message(MessageId.Control.StopRecordingCommand), _selectItem1.FQID);
+                    new Message(MessageId.Control.StopRecordingCommand), _selectItem1.FQID);
         }
 
         private void checkBoxHeader_Checked(object sender, RoutedEventArgs e)
@@ -366,7 +323,7 @@ namespace AlarmVideo
             _imageViewerWpfControl.AdaptiveStreaming = checkBoxAdaptiveStreaming.IsChecked.Value;
         }
 
-        private void alarmDetailsTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        private void alarmDetailsTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             // Create a new alarm object with the desired properties
             Alarm newAlarm = new Alarm
@@ -378,47 +335,5 @@ namespace AlarmVideo
             // Add the new alarm to the list of alarms
             alarmsListBox.Items.Add(newAlarm);
         }
-
-       
-
-        //private async void InitializeMap()
-        //{
-        //    // Connect to the Milestone Map Service
-        //    MapServiceClient mapServiceClient = new MapServiceClient();
-        //    await mapServiceClient.ConnectAsync();
-
-        //    // Set the map service as the geographic background for the Map control
-        //    MapView.MapService = mapServiceClient;
-
-        //    // Retrieve the camera information and coordinates from the Milestone management client
-        //    List<Camera> cameras = await GetCamerasAsync();
-
-        //    // Add camera icons to the map at the correct location
-        //    foreach (Camera camera in cameras)
-        //    {
-        //        MapIcon mapIcon = new MapIcon
-        //        {
-        //            Location = new Location(camera.Latitude, camera.Longitude),
-        //            Title = camera.Name,
-        //            Description = camera.Description
-        //        };
-
-        //        CameraIcons.Items.Add(mapIcon);
-        //    }
-        //}
-
-        //private async Task<List<Camera>> GetCamerasAsync()
-        //{
-        //    // Replace this with your actual code to retrieve the camera information and coordinates from the Milestone management client
-        //    List<Camera> cameras = new List<Camera>
-        //    {
-        //        new Camera { Name = "Camera 1", Description = "Description 1", Latitude = 52.370216, Longitude = 4.895168 },
-        //        new Camera { Name = "Camera 2", Description = "Description 2", Latitude = 52.370216, Longitude = 4.895168 }
-        //    };
-
-        //    return cameras;
-        //}
-
-
     }
 }
